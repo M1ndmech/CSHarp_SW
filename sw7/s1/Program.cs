fc9f80c9951540c4f64a8bac3ce8f13a7bb86d76
Mindmech <alex8008@bk.ru>
2022-12-11 10:28:56 +0300

    Matrix task 1 done

---
+++ sw7/s1/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// двумерный массив размерности m*n, заполненный целыми числами
+
+void PrintMatrix (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+            Console.WriteLine();
+    }
+}
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message}>");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int [,] GenerateMatrix (int rows, int columns, int minRange, int maxRange)
+{
+    int [,] array = new int [rows, columns];
+    for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i,j] = new Random().Next(minRange, maxRange+1);
+            }
+        }
+    return array;
+}
+
+int rowsInput = Prompt("Введите количество строк массива");
+int columnsInput = Prompt("Введите количество столбцов массива");
+int min = Prompt("Введите минимальное значение для ячеек массива");
+int max = Prompt("Введите максимальное значение для ячеек массива");
+
+int [,] newMatrix = GenerateMatrix(rowsInput, columnsInput, min, max);
+PrintMatrix (newMatrix);
+
